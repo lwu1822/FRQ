16cd2a2590ac1b93f5092a5348ca41351fd4bd43
lwu1822 <lwu1822@gmail.com>
2022-11-15 19:32:32 -0800

    change private to public to access from Year.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/APCalendar.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/APCalendar.java
@@ -30,7 +30,7 @@ public class APCalendar {
      * 6 denotes Saturday. 
      * firstDayOfYear(2019) returns 2 for Tuesday.
     */
-    private static int firstDayOfYear(int year) {
+    public static int firstDayOfYear(int year) {
         // implementation not shown
         int firstDay = dayOfWeek(1, 1, year);
         return firstDay; 
@@ -44,7 +44,7 @@ public class APCalendar {
      * dayOfYear(3, 1, 2017) returns 60, since 2017 is not a leap year
      * dayOfYear(3, 1, 2016) returns 61, since 2016 is a leap year. 
     */ 
-    private static int dayOfYear(int month, int day, int year) {
+    public static int dayOfYear(int month, int day, int year) {
         // implementation not shown
 
         boolean leapYear = isLeapYear(year);
