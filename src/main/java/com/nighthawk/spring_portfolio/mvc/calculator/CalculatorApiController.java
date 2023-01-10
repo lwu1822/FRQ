29ec8f649fe53323df92d141a40c439aa1ca57a0
lwu1822 <lwu1822@gmail.com>
2023-01-09 21:18:16 -0800

    CalculatorApiController.java: Division sign for API

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorApiController.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 
@@ -29,7 +32,7 @@ public class CalculatorApiController {
 */
    
 
-   
+   /* 
     @GetMapping("/{expression}")
     public ResponseEntity<JsonNode> getIsLeapYear(@PathVariable String expression) throws JsonMappingException, JsonProcessingException {
       // Backend Year Object
@@ -42,6 +45,21 @@ public class CalculatorApiController {
 
       return ResponseEntity.ok(json);  // JSON response, see ExceptionHandlerAdvice for throws
       
+    }
+    */
+
+    @GetMapping("all/**")
+    public ResponseEntity<JsonNode> allDirectories(HttpServletRequest request) throws JsonMappingException, JsonProcessingException {
+
+        String test = request.getRequestURI().split(request.getContextPath() + "/all/")[1];
+      
+         Calculator calculator_obj = new Calculator(test);
+      // Turn Year Object into JSON
+      ObjectMapper mapper = new ObjectMapper(); 
+      JsonNode json = mapper.readTree(calculator_obj.toStringJson()); // this requires exception handling
+
+      return ResponseEntity.ok(json); 
+        //return request.getRequestURI().split(request.getContextPath() + "/all/")[1];
     }
 
     // add other methods
