9093cfec6aca0db3f1f9c9e06111c6cc6dafd47e
lwu1822 <lwu1822@gmail.com>
2023-01-26 10:57:05 -0800

    PersonApiController.java: Hash pass w/ BCrypt

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -74,6 +75,7 @@ public class PersonApiController {
                                              @RequestParam("dob") String dobString, 
                                              @RequestParam("height") Double height) {
         Date dob;
+        password = BCrypt.hashpw(password, BCrypt.gensalt());
         // IMPORTANT: convert dobString f string to Date
         // try catch: if code in try statement mk error, x catch statement
         // c only do SimpleDataFormat in try catch statement!! 
