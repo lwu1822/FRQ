7b9cdae80568c5e9cf451fe285553fcf5019a495
VidhiKulkarni <vidhi.kulkarni@gmail.com>
2023-02-07 00:32:19 -0800

    finished person database backend

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -33,8 +33,6 @@ import java.util.Locale;
 
 import java.util.Map.Entry;
 
-
-
 /*
 Person is a POJO, Plain Old Java Object.
 First set of annotations add functionality to POJO
@@ -46,9 +44,9 @@ The last annotation connect to database
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@TypeDef(name="json", typeClass = JsonType.class)
+@TypeDef(name = "json", typeClass = JsonType.class)
 public class Person {
-    
+
     // automatic unique identifier for Person record
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -56,40 +54,40 @@ public class Person {
 
     // email, password, roles are key attributes to login and authentication
     @NotEmpty
-    @Size(min=5)
-    @Column(unique=true)
+    @Size(min = 5)
+    @Column(unique = true)
     @Email
     private String email;
 
     @NotEmpty
     private String password;
 
-    // @NonNull, etc placed in params of constructor: "@NonNull @Size(min = 2, max = 30, message = "Name (2 to 30 chars)") String name"
+    // @NonNull, etc placed in params of constructor: "@NonNull @Size(min = 2, max =
+    // 30, message = "Name (2 to 30 chars)") String name"
     @NonNull
-    @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
+    @Size(min = 2, max = 30, message = "Name (2 to 35 chars)")
     private String name;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
 
     // IMPORTANT: double n captilize for some reason
-    private Double height; 
+    private Double height;
 
-    private String role; 
-    
+    private String role;
 
-    /* HashMap is used to store JSON for daily "stats"
-    "stats": {
-        "2022-11-13": {
-            "calories": 2200,
-            "steps": 8000
-        }
-    }
-    */
-    @Type(type="json")
+    /*
+     * HashMap is used to store JSON for daily "stats"
+     * "stats": {
+     * "2022-11-13": {
+     * "calories": 2200,
+     * "steps": 8000
+     * }
+     * }
+     */
+    @Type(type = "json")
     @Column(columnDefinition = "jsonb")
-    private Map<String,Map<String, Object>> stats = new HashMap<>(); 
-    
+    private Map<String, Map<String, Object>> stats = new HashMap<>();
 
     // Constructor used when building object from an API
     public Person(String email, String password, String name, Date dob, Double height, String role) {
@@ -97,161 +95,153 @@ public class Person {
         this.password = password;
         this.name = name;
         this.dob = dob;
-        this.height = height; 
-        this.role = role; 
+        this.height = height;
+        this.role = role;
     }
 
     // A custom getter to return age from dob attribute
     public int getAge() {
         if (this.dob != null) {
-            // IMPORTANT: convert dob to current zone, then remove all the junk, leave behind 
+            // IMPORTANT: convert dob to current zone, then remove all the junk, leave
+            // behind
             // YYYY-MM-DD
             LocalDate birthDay = this.dob.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
             // Calculate num year, month, day, btw dob + today -> sh # years
-            return Period.between(birthDay, LocalDate.now()).getYears(); }
+            return Period.between(birthDay, LocalDate.now()).getYears();
+        }
         return -1;
     }
-    
 
-/* 
-    public String emailToString() {
-      return (  "" + this.statsTwo );
+    /*
+     * public String emailToString() {
+     * return ( "" + this.statsTwo );
+     * 
+     * // return entire person object
+     * //return ("Person(id=" + this.id + ", email=" + this.email + ", password=" +
+     * this.password + ", name=" + this.name + ", dob=" + this.dob + ", stats=" +
+     * this.stats + ", thirdMap=" + this.thirdMap + ")" );
+     * }
+     */
 
-        // return entire person object
-      //return ("Person(id=" + this.id + ", email="  + this.email + ", password=" + this.password + ", name=" + this.name + ", dob=" + this.dob + ", stats=" + this.stats + ", thirdMap=" + this.thirdMap + ")" );
-    }
-    */ 
-
-    
-    public Map<String, Map<String, Object>> toStringNotDefault() { 
+    public Map<String, Map<String, Object>> toStringNotDefault() {
         return this.statsTwo;
-     }
+    }
 
-     public Map<String, Map<String, Object>> toStringNotDefaultNewStats() { 
-        return this.stats; 
-     }
+    public Map<String, Map<String, Object>> toStringNotDefaultNewStats() {
+        return this.stats;
+    }
 
-
-    @Type(type="json")
+    @Type(type = "json")
     @Column(columnDefinition = "jsonb")
     Map<String, Map<String, Object>> statsTwo = new HashMap<>();
- 
-     public void setStatsTwo(Map<String, Map<String, Object>> statsTwo) {  
 
-        this.statsTwo = statsTwo; 
-    } 
+    public void setStatsTwo(Map<String, Map<String, Object>> statsTwo) {
 
-    @Type(type="json")
+        this.statsTwo = statsTwo;
+    }
+
+    @Type(type = "json")
     @Column(columnDefinition = "jsonb")
     Map<String, Map<String, Object>> workout = new HashMap<>();
 
-    
-
     public String toString() {
-        return ("Person(id=" + this.id + ", email=" + this.email +", password=" + this.password + ", name=" + this.name + ", dob=" + this.dob + ", stats=" + this.stats + ", statsTwo=" + this.statsTwo + ", workout=" + this.workout + ")");
+        return ("Person(id=" + this.id + ", email=" + this.email + ", password=" + this.password + ", name=" + this.name
+                + ", dob=" + this.dob + ", stats=" + this.stats + ", statsTwo=" + this.statsTwo + ", workout="
+                + this.workout + ")");
     }
 
-
     public static void main(String[] args) {
-  
-        // IMPORTANT: If only do Date dob;, will h error: the local variable (var) may not have been initialized
-        //  https://stackoverflow.com/questions/2448843/variable-might-not-have-been-initialized-error
-        Date dob = null; 
-    
+
+        // IMPORTANT: If only do Date dob;, will h error: the local variable (var) may
+        // not have been initialized
+        // https://stackoverflow.com/questions/2448843/variable-might-not-have-been-initialized-error
+        Date dob = null;
+
         try {
             dob = new SimpleDateFormat("MM-dd-yyyy").parse("10-21-1900");
         } catch (Exception e) {
-            System.out.println("error"); 
+            System.out.println("error");
         }
 
-        Person personNoArg = new Person(); 
-        Person person = new Person("tedison@gmail.com", "123qwerty!", "Thomas Edison", dob, (Double)70.0, "admin"); 
+        Person personNoArg = new Person();
+        Person person = new Person("tedison@gmail.com", "123qwerty!", "Thomas Edison", dob, (Double) 70.0, "admin");
 
-        System.out.println("No arg constructor: " + personNoArg); 
+        System.out.println("No arg constructor: " + personNoArg);
 
-        System.out.println("Email: " + person.getEmail()); 
-        System.out.println("Password: " + person.getPassword()); 
-        System.out.println("Name: " + person.getName()); 
-        System.out.println("dob: " + person.getDob()); 
+        System.out.println("Email: " + person.getEmail());
+        System.out.println("Password: " + person.getPassword());
+        System.out.println("Name: " + person.getName());
+        System.out.println("dob: " + person.getDob());
         System.out.println("Age: " + person.getAge());
 
         // "original" stats
         Map<String, Map<String, Object>> stat = new LinkedHashMap<>();
-        Map<String,Object> statsub = new LinkedHashMap<>(); 
-        statsub.put("steps", (Integer)10000); 
-        statsub.put("calories", (Integer)2000); 
-        statsub.put("sleep", (Integer)8); 
+        Map<String, Object> statsub = new LinkedHashMap<>();
+        statsub.put("steps", (Integer) 10000);
+        statsub.put("calories", (Integer) 2000);
+        statsub.put("sleep", (Integer) 8);
 
-        stat.put("11-20-2022", statsub); 
+        stat.put("11-20-2022", statsub);
 
-        Map<String,Map<String, Object>> stat2 = new HashMap<>();
-        Map<String,Object> statsub2 = new LinkedHashMap<>(); 
-        statsub2.put("steps", (Integer)8000); 
-        statsub2.put("calories", (Integer)2000); 
-        statsub2.put("sleep", (Integer)9); 
+        Map<String, Map<String, Object>> stat2 = new HashMap<>();
+        Map<String, Object> statsub2 = new LinkedHashMap<>();
+        statsub2.put("steps", (Integer) 8000);
+        statsub2.put("calories", (Integer) 2000);
+        statsub2.put("sleep", (Integer) 9);
         stat2.put("11-21-2022", statsub2);
 
-        //System.out.println(stat); 
-        //System.out.println(stat2); 
+        // System.out.println(stat);
+        // System.out.println(stat2);
 
-       
         person.setStats(stat);
 
-        Map<String,Map<String, Object>> stat3 = new LinkedHashMap<>();
-        for(Entry<String, Map<String, Object>> entry: stat.entrySet()) {
-            stat3.put(entry.getKey(), entry.getValue()); 
+        Map<String, Map<String, Object>> stat3 = new LinkedHashMap<>();
+        for (Entry<String, Map<String, Object>> entry : stat.entrySet()) {
+            stat3.put(entry.getKey(), entry.getValue());
         }
-        for(Entry<String, Map<String, Object>> entry: stat2.entrySet()) {
-            stat3.put(entry.getKey(), entry.getValue()); 
+        for (Entry<String, Map<String, Object>> entry : stat2.entrySet()) {
+            stat3.put(entry.getKey(), entry.getValue());
         }
 
-        person.setStatsTwo(stat3); 
-        //System.out.println(stat3); 
+        person.setStatsTwo(stat3);
+        // System.out.println(stat3);
 
-        System.out.println("toString: " + person.toString()); 
-    
+        System.out.println("toString: " + person.toString());
 
-        
-        
-         /* 
-        Map<String, Object> subMap = new HashMap<>(); 
-        subMap.put("calories", 2200); 
-        subMap.put("steps", 8000); 
+        /*
+         * Map<String, Object> subMap = new HashMap<>();
+         * subMap.put("calories", 2200);
+         * subMap.put("steps", 8000);
+         * 
+         * System.out.println(subMap);
+         * 
+         * Map<String,Map<String, Object>> actualMap = new HashMap<>();
+         * actualMap.put("2022-11-13", subMap);
+         * person.setStats(actualMap);
+         * 
+         * 
+         * 
+         * Map<String, Object> subMap2 = new HashMap<>();
+         * subMap2.put("calories", 1000);
+         * subMap2.put("steps", 5000);
+         * 
+         * System.out.println(subMap2);
+         * 
+         * Map<String,Map<String, Object>> actualMap2 = new HashMap<>();
+         * actualMap2.put("2022-11-14", subMap2);
+         * 
+         * 
+         * person.setStats(actualMap2);
+         * 
+         * System.out.println(person);
+         */
 
-        System.out.println(subMap); 
-
-        Map<String,Map<String, Object>> actualMap = new HashMap<>(); 
-        actualMap.put("2022-11-13", subMap);
-        person.setStats(actualMap);
-
-
-
-        Map<String, Object> subMap2 = new HashMap<>(); 
-        subMap2.put("calories", 1000); 
-        subMap2.put("steps", 5000); 
-
-        System.out.println(subMap2); 
-
-        Map<String,Map<String, Object>> actualMap2 = new HashMap<>(); 
-        actualMap2.put("2022-11-14", subMap2);
-
-
-        person.setStats(actualMap2);
-
-        System.out.println(person); 
-        */
-
-/* 
-       System.out.println("Stats: " + person.getStats()); 
-        Map<Integer,Map<String,Map<String, Object>>> thirdMap = new HashMap<>(); 
-        thirdMap.put(1, actualMap); 
-        System.out.println(thirdMap); 
-        */ 
-
-
-
-
-
+        /*
+         * System.out.println("Stats: " + person.getStats());
+         * Map<Integer,Map<String,Map<String, Object>>> thirdMap = new HashMap<>();
+         * thirdMap.put(1, actualMap);
+         * System.out.println(thirdMap);
+         */
 
     }
 
