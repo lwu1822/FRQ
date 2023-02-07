878de6bd6e25997e0cdfa1648eb116d548c8ff57
willcyber <wutwilliam@gmail.com>
2023-02-06 21:41:30 -0800

    trying

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/image/ImageService.java
@@ -0,0 +1,26 @@
+package com.nighthawk.spring_portfolio.mvc.image;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class ImageService {
+    private static final String UPLOADED_FOLDER = "/path/to/images/";
+
+    public boolean uploadImage(MultipartFile file) {
+        try {
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+}
