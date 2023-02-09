722b48e74f7487fcac2a8e1c593740a9c715d5ba
lwu1822 <lwu1822@gmail.com>
2023-02-09 09:43:46 -0800

    cors 5

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -6,25 +6,25 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    // set up your own index
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login/authenticate").setViewName("login");
-    }
+    // // set up your own index
+    // @Override
+    // public void addViewControllers(ViewControllerRegistry registry) {
+    //     registry.addViewController("/login/authenticate").setViewName("login");
+    // }
 
-    /* map path and location for "uploads" outside of application resources
-       ... creates a directory outside "static" folder, "file:volumes/uploads"
-       ... CRITICAL, without this uploaded file will not be loaded/displayed by frontend
-     */
-    @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/volumes/uploads/**").addResourceLocations("file:volumes/uploads/");
-    }
+    // /* map path and location for "uploads" outside of application resources
+    //    ... creates a directory outside "static" folder, "file:volumes/uploads"
+    //    ... CRITICAL, without this uploaded file will not be loaded/displayed by frontend
+    //  */
+    // @Override
+    // public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+    //     registry.addResourceHandler("/volumes/uploads/**").addResourceLocations("file:volumes/uploads/");
+    // }
 
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://nighthawkcoders.github.io", "http://localhost:4002");
+        registry.addMapping("/**").allowedOrigins("https://crimebusters.tk/login/");
     }
     
 }
