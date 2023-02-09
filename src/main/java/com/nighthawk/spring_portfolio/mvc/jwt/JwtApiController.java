5f62d223ff2167521d53e80b0a55eb9c52e6048c
lwu1822 <lwu1822@gmail.com>
2023-02-09 09:35:12 -0800

    cors 3

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
@@ -19,7 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.HttpStatus;
 
 @RestController
-@CrossOrigin(origins = "https://lwu1822.github.io/crimebustersrevival/login")
+@CrossOrigin
 public class JwtApiController {
 
 	@Autowired
