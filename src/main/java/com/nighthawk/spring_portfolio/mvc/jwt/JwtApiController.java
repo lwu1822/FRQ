cd6667a7729334544effbc7e43a35a80b2fa71b8
lwu1822 <lwu1822@gmail.com>
2023-02-09 19:47:32 -0800

    cors 16

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
@@ -20,7 +20,7 @@ import org.springframework.http.HttpStatus;
 
 @RestController
 //@CrossOrigin(origins={"https://lwu1822.github.io"})
-@CrossOrigin
+//@CrossOrigin
 public class JwtApiController {
 
 	@Autowired
