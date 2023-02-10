c38ce27a5e0b47da1e8351f4cd53c1343ec145b2
lwu1822 <lwu1822@gmail.com>
2023-02-09 21:04:19 -0800

    cors 19

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
@@ -41,7 +41,7 @@ public class JwtApiController {
 		final ResponseCookie tokenCookie = ResponseCookie.from("jwt", token)
 			.httpOnly(true)
 			.secure(true)
-			.path("/")
+			.path("/login")
 			.maxAge(3600)
 			.sameSite("None; Secure")
 			// .domain("example.com") // Set to backend domain
